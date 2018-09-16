d21efb13c2f38b847ede6c52527fdb6320212434
viymak <viymak@icloud.com>
2018-09-16 00:06:39 -0400

    added test page load

--- code_gov_web_tester/__init__.py
+++ code_gov_web_tester/__init__.py
@@ -1,7 +1,25 @@
-import selenium
+from selenium import webdriver
 import unittest
 
+class TestPageLoad(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.addCleanup(self.browser.quit)
+
+    def testPageTitle(self):
+        self.browser.get('https://code.gov')
+        self.assertIn('code.gov', self.browser.title)
+
+'''
 class TestHomeArea(unittest.TestCase):
 
-    def fake_test(self):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.addCleanup(self.browser.quit)
+
+    def test_fake(self):
         self.assertEqual(1,1)
+'''
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
